03ed899f7160a7d62f6bfac9748bf824bf326cec
Arpit <arpitvermaetw@gmail.com>
2024-05-28 22:18:34 +0530

    Change the Footer to center div and logos

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -44,7 +44,7 @@ const Footer = () => {
           </div>
       </div>
       <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-      <div className="sm:flex sm:items-center sm:justify-between">
+      <div className="sm:flex sm:items-center sm:justify-between text-center">
           <div className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://instagram.com/" target='_blank' className="hover:underline">iNoteBook</a>. All Rights Reserved.
           </div>
 
@@ -53,7 +53,7 @@ const Footer = () => {
           </div>
 
           
-          <div className="flex mt-4 sm:justify-center sm:mt-0">
+          <div className="flex mt-4 sm:justify-center sm:mt-0 justify-center">
               <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
                   <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
                         <path fillRule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clipRule="evenodd"/>
